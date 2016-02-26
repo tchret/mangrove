1ea785334453c6d60e2f2a4a17c17a21c0927042
mx.braud@gmail.com <mx.braud@gmail.com>
2016-02-26 13:44:04 +0100

    Automated commit at 2016-02-26 12:44:04 UTC by middleman-deploy 1.0.0

---
+++ javascripts/home.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  $("#cantattend").click(function() {
+    $("#periscope").toggleClass("hidden");
+    if($("#cantattend").text() == "Got it!") {
+      $("#cantattend").text("Show me again!");
+    }
+    else {
+      $("#cantattend").text("Got it!");
+    }
+  });
+
+  $("#snaphover").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  //   $("#staytuned").toggle(
+  //     function(){$(".section-heading").css({"visibility": "hidden"});},
+  //     function(){$("#staytuned").css({"opacity": "0"});
+  //   });
+  });
+
+  $("#snapcode").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  });
+
+  $("#snapcode-bg").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  });
+
+});
